c3b8b3f68ba811d58fb337da3e75621464c4a0ea
AndrewKralovec <akrala@yahoo.com>
2020-07-11 17:00:07 -0500

    add more help

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def parse_args(args):
     parser.add_argument('--file_name', help='',
                         required=False, default='output')
     # Request options
-    parser.add_argument('--year_min', help='',
+    parser.add_argument('--year_min', help='starting value for the years range in the search query',
                         required=False, default=2018)
-    parser.add_argument('--year_max', help='',
+    parser.add_argument('--year_max', help='ending value for the years range in the search query',
                         required=False, default=2019)
     parser.add_argument('--pos', '--position', nargs='+',
-                        help='', required=False, default=['QB', 'WR', 'RB', 'TE', 'OL', 'DL', 'LB', 'DB'])
+                        help='positions for the search query. Defaults to all postions', required=False, default=['QB', 'WR', 'RB', 'TE', 'OL', 'DL', 'LB', 'DB'])
     parser.add_argument('--game_num_min', help='',
                         required=False, default=0)
     parser.add_argument('--game_num_max', help='',
@@ -24,7 +24,7 @@ def parse_args(args):
                         required=False, default=0)
     parser.add_argument('--week_num_max', help='',
                         required=False, default=99)
-    parser.add_argument('--order_by', help='',
+    parser.add_argument('--order_by', help='What score/points segment the query will be ordered by',
                         required=False, default='fantasy_points')
     return parser.parse_args(args)
 
