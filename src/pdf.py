5cc46f16c6b4843a602e2c9cc13245854ad6c890
AndrewKralovec <akrala@yahoo.com>
2020-07-11 21:41:08 -0500

    insert headers into data

--- src/pdf.py
+++ src/pdf.py
@@ -6,13 +6,13 @@ from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
 def main(file_name, table):
     (headers, data) = table
+    data.insert(0, headers)
     (w, h) = letter
     l = (w + (len(data[0]) * 3) * 15, h + len(data) * 15)
     doc = SimpleDocTemplate(file_name, pagesize=l)
     # container for the 'Flowable' objects
     elements = []
 
-    # data = [headers, rows]
     t = Table(data, len(data[0])*[0.85*inch], len(data)*[0.85*inch])
     t.setStyle(TableStyle([('LINEABOVE', (0, 0), (-1, 0), 2, colors.green),
                            ('LINEABOVE', (0, 1), (-1, -1), 0.25, colors.black),
@@ -20,9 +20,8 @@ def main(file_name, table):
                            ('ALIGN', (1, 1), (-1, -1), 'RIGHT')]))
 
     elements.append(t)
-    # write the document to disk
-    out = doc.build(elements)
-    print
+    # Write the document to disk
+    doc.build(elements)
     return ('done')
 
 
